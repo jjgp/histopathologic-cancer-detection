beed5ef14f84f0489f44e7546494d830d72526d9
Jason Prasad <jasongprasad@gmail.com>
2021-11-04 17:00:10 +0000

    Update to python path and image scans

--- lib/dataset.py
+++ lib/dataset.py
@@ -1,6 +1,7 @@
 import os
 
 import cv2
+import numpy as np
 import pandas as pd
 
 
@@ -10,9 +11,10 @@ def rgb_imread(filepath):
 
 
 class Dataset(object):
-    def __init__(self, data_dirpath, csv_filename, images_dirname):
+    def __init__(self, data_dirpath, csv_filename, images_dirname, npy_filename):
         self.csv_filepath = os.path.join(data_dirpath, csv_filename)
         self.images_dirpath = os.path.join(data_dirpath, images_dirname)
+        self.npy_filepath = os.path.join(data_dirpath, npy_filename)
 
     def read_csv(self):
         return pd.read_csv(self.csv_filepath)
@@ -21,17 +23,22 @@ class Dataset(object):
         return os.path.join(self.images_dirpath, id + ".tif")
 
     def read_images_and_labels(self, labels_df):
-        image_paths = labels_df["id"].map(self.image_path)
-        images = image_paths.map(rgb_imread).to_numpy()
+        images = None
+        if os.path.exists(self.npy_filepath):
+            images = np.load(self.npy_filepath, allow_pickle=True)
+        else:
+            image_paths = labels_df["id"].map(self.image_path)
+            images = image_paths.map(rgb_imread).to_numpy()
+            np.save(self.npy_filepath, images)
         labels = labels_df["label"].to_numpy()
         return images, labels
 
 
 class TrainDataset(Dataset):
     def __init__(self, data_dirname):
-        super().__init__(data_dirname, "train_labels.csv", "train")
+        super().__init__(data_dirname, "train_labels.csv", "train", "train.npy")
 
 
 class TestDataset(Dataset):
     def __init__(self, data_dirname):
-        super().__init__(data_dirname, "sample_submission.csv", "test")
+        super().__init__(data_dirname, "sample_submission.csv", "test", "test.npy")
